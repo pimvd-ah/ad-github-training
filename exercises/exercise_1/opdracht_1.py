2f8aacc433d24edede8627e8dd7c63eeecd3a5b1
lcrijnenah <Louisa.Crijnen@ah.nl>
2025-11-11 15:19:14 +0000

    Add like function

--- exercises/exercise_1/opdracht_1.py
+++ exercises/exercise_1/opdracht_1.py
@@ -1,2 +1,8 @@
 def likes(team: list) -> str:
-    return True
+
+    if len(list) == 0:
+        return "no one likes this"
+    elif len(list) == 1:
+        return "{list[0]} likes this"
+    elif len(list) == 2:
+        return "{list[0]} and {list[1]} like this"
